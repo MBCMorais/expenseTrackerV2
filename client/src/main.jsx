3c77448a0dad0885ae110dbc97501c02a63efac0
DisturbedMindSet <kl4usmik4elsonn@gmail.com>
2023-04-04 06:48:15 +0100

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,6 +7,10 @@ import './index.css';
 import Layout from './pages/Layout';
 import Home from './pages/Home';
 import About from './pages/About';
+import Settings from "./pages/Settings"
+import Calendar from "./pages/Calendar"
+import Expense from "./pages/Expense"
+import Income from "./pages/Income"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 	<React.StrictMode>
@@ -15,6 +19,10 @@ ReactDOM.createRoot(document.getElementById('root')).render(
 				<Route path='/' element={<Layout />}>
 					<Route index element={<Home />} />
 					<Route path='about' element={<About />} />
+					<Route path='expense' element={<Expense />} />
+					<Route path='income' element={<Income />} />
+					<Route path='calendar' element={<Calendar />} />
+					<Route path='settings' element={<Settings />} />
 					{/* <Route path='contact' element={<Contact />} /> */}
 					{/* <Route path='*' element={<NoPage />} /> */}
 				</Route>
