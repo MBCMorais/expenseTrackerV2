45ad8b2fe0c8ba659192a51e49eccd3f9e7f2a66
DisturbedMindSet <kl4usmik4elsonn@gmail.com>
2023-04-04 06:48:00 +0100

    Update Layout.jsx

--- client/src/pages/Layout.jsx
+++ client/src/pages/Layout.jsx
@@ -1,28 +1,22 @@
 import React from 'react';
 import MySidebar from '../components/Sidebar';
 import { Outlet } from 'react-router-dom';
-import { Container, Row, Col, } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
 import Footer from '../components/Footer';
-import SidebarV2 from '../components/SidebarV2';
 
 
 const Layout = () => {
 	return (
 		<>
-			<div className='vh-100 vw-100'>
-                
-				<div className='p-0 d-flex'>
-					<Col md={3} >
-                    <MySidebar />
-                        {/* <SidebarV2 /> */}
-					</Col>
-					<Col md={9}>
-						<Outlet />
-					</Col>
-				</div>
-                <Footer/>
+			<div id='responsiveTablet' className='vh-100 vw-100 p-0 d-flex '>
+				<Col md={3} xl={2}>
+					<MySidebar />
+				</Col>
+				<Col md={9} xl={10}>
+					<Outlet />
+					<Footer />
+				</Col>
 			</div>
-           
 		</>
 	);
 };
